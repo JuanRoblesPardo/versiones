7be93a97cb79500431dc5292e6396453f3b85dbb
JuanRoblesPardo <juanfeliperobles@hotmail.com>
2023-09-10 13:17:19 -0300

    nuevorp

---
+++ src/practicagh1/PracticaGH1.java
@@ -0,0 +1,11 @@
+
+package practicagh1;
+
+public class PracticaGH1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("prueba linea gui hub");
+    }
+    
+}
