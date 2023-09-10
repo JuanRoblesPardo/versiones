fc9626d01cd4b724cf55e2a4a0bf96059ac9d0d2
JuanRoblesPardo <juanfeliperobles@hotmail.com>
2023-09-10 16:41:48 -0300

    prueba juan

--- src/practicagh1/PracticaGH1.java
+++ src/practicagh1/PracticaGH1.java
@@ -5,7 +5,7 @@ public class PracticaGH1 {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("prueba linea gui hub");
+        System.out.println("prueba linea guijuan hub");
     }
     
 }
